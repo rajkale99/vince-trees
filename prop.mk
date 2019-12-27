cb8e9bec8829ec35fe68188fb1e4d23e7e341177
Roker2 <dimaminko2000@gmail.com>
2019-12-27 17:37:24 +0530

    Fix Lags On Live Wallpapers

--- prop.mk
+++ prop.mk
@@ -92,7 +92,8 @@ debug.enable.sglscale=1 \
 debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.enable_hwc_vds=1 \
-debug.sf.latch_unsignaled=1 \
+debug.sf.hw=0 \
+debug.sf.latch_unsignaled=0 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
@@ -286,7 +287,6 @@ TapSlop=1px
 
 # Lau's magic props
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.hw=1 \
 debug.hwui.renderer=skiagl \
 debug.cpurend.vsync=false \
 vendor.display.enable_default_color_mode=0 \
