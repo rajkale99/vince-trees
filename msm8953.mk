637d00e7a1f6b55f5a7b2f9e7439b834509752da
rajkale99 <kaleraj.rk@gmail.com>
2020-07-09 11:57:04 +0000

    Revert "msm8953-common: Build CAF power HIDL HAL"
    This reverts commit c27795a9fb477fe5072a6cfde0f94c171460bd10.

--- msm8953.mk
+++ msm8953.mk
@@ -269,9 +269,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-impl \
-    android.hardware.power@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
